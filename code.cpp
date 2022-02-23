e871b7996f5e84d8b0e9f40867f97110d5549c53
FIRST_NAME LAST_NAME <khadijarejjaoui41@gmail.com>
2022-02-23 19:12:28 +0600

    first commit

---
+++ code.cpp
@@ -0,0 +1,11 @@
+//Solution to problem 1000 - Hello World! in beecrowd
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    cout << "Hello World!" <<endl;
+    system("pause");
+    return 0;
+}
